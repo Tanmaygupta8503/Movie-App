12ac08e89efeb60e97e8d1f0de26efa1524fd4ac
Tanmay Gupta <96345346+Tanmaygupta8503@users.noreply.github.com>
2023-10-01 18:52:54 +0530

    Movie App Via React
    Movie App Completed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import MovieCard from './MovieCard';
+import MovieList from './MovieList';
+function App() {
+  return (
+    <>
+      <h1>
+        Movie App
+      </h1>
+      {/* <MovieCard /> */}
+      <MovieList/>
+    </>
+  );
+}
+
+export default App;
